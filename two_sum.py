6fc95bd6158760f14934831eb6ecfebbc6654482
Joe Green <joseph.peter.green@gmail.com>
2024-07-30 08:46:53 +0100

    feat(leetcode): longest consecutive solution

--- two_sum.py
+++ two_sum.py
@@ -2,12 +2,17 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # create hash map to store previous values
         prevMap = {}
 
+        # iterate through the list
         for i, n in enumerate(nums):
+            # check if the difference is in the hash map
             diff = target - n
             if diff in prevMap:
+                # return current index and index of the difference as it totals the target
                 return [prevMap[diff], i]
+            # store the current value in the hash map and it's index
             prevMap[n] = i 
 
         #   {
